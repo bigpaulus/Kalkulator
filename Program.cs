c302c87bf6c9e1d289c099bb2697a1761c5b2962
bigpaulus <paulsarnecki@gmail.com>
2020-02-07 22:26:06 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą liczbę do działania: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj drugą liczbę do działania: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Plus(a , b);
+            
+
+            Console.ReadKey();
+        }
+
+        static void Plus(int a, int b)
+        {
+            int dodawanie = a + b;
+            Console.WriteLine("Wynik to: ");
+            Console.WriteLine(dodawanie);
+        }
+
+
+
+
+    }
+}
+
