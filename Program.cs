21a96bfdef865d4592e5eb90ec25e002eb469d13
bigpaulus <paulsarnecki@gmail.com>
2020-02-07 22:42:23 +0100

    Dodanie do kalkulatora funkcji działań. (+ , - , / , *)

--- Program.cs
+++ Program.cs
@@ -13,14 +13,41 @@ namespace Kalkulator
             Console.WriteLine("Podaj pierwszą liczbę do działania: ");
             int a = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Podaj rodzaj działania(+, -, /, *)");
+            string c = Console.ReadLine();
+
             Console.WriteLine("Podaj drugą liczbę do działania: ");
             int b = int.Parse(Console.ReadLine());
 
-            Plus(a , b);
-            
+
+            if (c=="+")
+            {
+                Plus(a, b);
+            }
+            if (c=="-")
+            {
+                Minus(a, b);
+            }
+            if (c=="*")
+            {
+                Razy(a, b);
+            }
+            if (c=="/")
+            {
+                Przez(a, b);
+            }
+            else
+            {
+                Console.WriteLine("Wpisz jeden z czterech podanych znaków (+, -, /, *)");
+            }
+
+
+           
 
             Console.ReadKey();
         }
+
+
 
         static void Plus(int a, int b)
         {
@@ -29,8 +56,26 @@ namespace Kalkulator
             Console.WriteLine(dodawanie);
         }
 
+        static void Minus(int a, int b)
+        {
+            int odejmowanie=(a-b);
+            Console.WriteLine("Wynik to: ");
+            Console.WriteLine(odejmowanie);
+        }
 
+        static void Razy(int a, int b)
+        {
+            int mnozenie = (a * b);
+            Console.WriteLine("Wynik to: ");
+            Console.WriteLine(mnozenie);
+        }
 
+        static void Przez(int a, int b)
+        {
+            int dzielenie = (a / b);
+            Console.WriteLine("Wynik to: ");
+            Console.WriteLine(dzielenie);
+        }
 
     }
 }
